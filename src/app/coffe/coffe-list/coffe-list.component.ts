0cb58123978fe87c3eb45b288307589823ba72b6
Sergio Gelvez <desarrollo@osiriscloud.co>
2024-11-03 18:53:14 -0500

    Implementación de conteo por tipo de cafe

--- src/app/coffe/coffe-list/coffe-list.component.ts
+++ src/app/coffe/coffe-list/coffe-list.component.ts
@@ -10,16 +10,36 @@ import { CoffeeService } from '../coffee.service';
 export class CoffeListComponent implements OnInit {
 
   coffees: Array<Coffee> = [];
+  conteoXTipo: Map<string, number> = new Map();
   constructor(private coffeeService: CoffeeService) { }
 
   getCoffees():void{
     this.coffeeService.getCoffees().subscribe((coffees) => {
       this.coffees = coffees;
+      this.countTypeCoffee();
     });
   }
 
+  countTypeCoffee():void{
+    console.log(this.coffees.length)
+    for (let i = 0; i < this.coffees.length; i++) {
+      let tipo = this.coffees[i].tipo; 
+      tipo = tipo.replace("Café","");     
+      let valor : number | undefined = 0;
+      if(this.conteoXTipo.has(tipo)){
+        valor =  this.conteoXTipo.get(tipo);
+        if(valor == undefined){
+          valor = 1;
+        }
+        this.conteoXTipo.set(tipo,valor+1);
+      }else{
+        this.conteoXTipo.set(tipo,1);
+      }
+    }
+  }
   ngOnInit() {
     this.getCoffees();
+    
   }
 
 }
